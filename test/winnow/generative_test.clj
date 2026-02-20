4f1699462a47ed76fe9a42d6b31e9f18847abc70
James Conroy-Finn <james@invetica.co.uk>
2026-02-20 20:27:58 +0000

    Use data-driven generators in generative tests

--- test/winnow/generative_test.clj
+++ test/winnow/generative_test.clj
@@ -6,89 +6,12 @@
    [clojure.test.check.generators :as gen]
    [clojure.test.check.properties :refer [for-all]]
    [winnow.api :as api]
-   [winnow.config :as config]))
+   [winnow.generators :as g]))
 
 (def ^:private num-tests 100)
 
 ;;; ----------------------------------------------------------------------------
-;;; Generators
-
-(def gen-color
-  (gen/elements (vec (:colors config/default))))
-
-(def gen-shade
-  (gen/elements ["50" "100" "200" "300" "400" "500" "600" "700" "800" "900" "950"]))
-
-(def gen-color-with-shade
-  (gen/fmap (fn [[c s]] (str c "-" s))
-            (gen/tuple gen-color gen-shade)))
-
-(def gen-opacity
-  (gen/elements ["/0" "/5" "/10" "/25" "/50" "/75" "/90" "/100"]))
-
-(def gen-color-value
-  (gen/one-of
-   [gen-color
-    gen-color-with-shade
-    (gen/fmap (fn [[c o]] (str c o))
-              (gen/tuple gen-color-with-shade gen-opacity))]))
-
-(def gen-spacing
-  (gen/elements ["0" "1" "2" "3" "4" "5" "6" "8" "10" "12" "16" "20" "24" "32"
-                 "px" "0.5" "1.5" "2.5" "3.5" "auto" "full"]))
-
-(def gen-css-unit
-  (gen/elements ["px" "rem" "em" "%" "vh" "vw"]))
-
-(def gen-arbitrary-length
-  (gen/fmap (fn [[n u]] (str "[" n u "]"))
-            (gen/tuple (gen/choose 1 999) gen-css-unit)))
-
-(def gen-hex-color
-  (gen/fmap (fn [[r g b]] (format "[#%02x%02x%02x]" r g b))
-            (gen/tuple (gen/choose 0 255)
-                       (gen/choose 0 255)
-                       (gen/choose 0 255))))
-
-(def gen-arbitrary-color
-  "Arbitrary color values that should be recognized as colors."
-  (gen/one-of
-   [gen-hex-color
-    (gen/fmap #(str "[rgb(" % ",0,0)]") (gen/choose 0 255))
-    (gen/fmap #(str "[hsl(" % "_80%_50%)]") (gen/choose 0 360))]))
-
-(def gen-arbitrary-width
-  "Arbitrary width values that should NOT be recognized as colors."
-  (gen/one-of
-   [gen-arbitrary-length
-    (gen/fmap #(str "[" % "]") (gen/choose 1 10))]))
-
-(def gen-breakpoint
-  (gen/elements ["sm" "md" "lg" "xl" "2xl"]))
-
-(def gen-pseudo
-  (gen/elements ["hover" "focus" "active" "disabled" "first" "last"
-                 "focus-within" "focus-visible"]))
-
-(def gen-modifier
-  (gen/one-of [gen-pseudo
-               gen-breakpoint
-               (gen/return "dark")
-               (gen/fmap #(str "group-" %) gen-pseudo)
-               (gen/fmap #(str "peer-" %) gen-pseudo)]))
-
-;;; ----------------------------------------------------------------------------
-;;; Utility generators
-
-(def gen-spacing-utility
-  (gen/fmap (fn [[p v]] (str p "-" v))
-            (gen/tuple (gen/elements ["p" "px" "py" "m" "mx" "my" "w" "h" "gap"])
-                       (gen/one-of [gen-spacing gen-arbitrary-length]))))
-
-(def gen-color-utility
-  (gen/fmap (fn [[p c]] (str p "-" c))
-            (gen/tuple (gen/elements ["text" "bg" "border" "ring" "shadow"])
-                       (gen/one-of [gen-color-value gen-hex-color]))))
+;;; Derived generators
 
 (def gen-display
   (gen/elements ["block" "inline-block" "inline" "flex" "grid" "hidden"]))
@@ -96,84 +19,31 @@
 (def gen-position
   (gen/elements ["static" "fixed" "absolute" "relative" "sticky"]))
 
-(def gen-base-class
-  (gen/one-of [gen-spacing-utility
-               gen-color-utility
-               gen-display
-               gen-position]))
-
-(def gen-tailwind-class
-  (gen/let [mods (gen/vector gen-modifier 0 2)
-            base gen-base-class]
-    (if (seq mods)
-      (str (str/join ":" mods) ":" base)
-      base)))
-
-(def gen-tailwind-class-list
-  (gen/vector gen-tailwind-class 1 8))
-
-;;; ----------------------------------------------------------------------------
-;;; Conflict generators
-
-(def gen-conflicting-colors
-  (gen/fmap (fn [[p c1 c2]] [(str p "-" c1) (str p "-" c2)])
-            (gen/tuple (gen/elements ["text" "bg" "border"])
-                       gen-color-value
-                       gen-color-value)))
-
-(def gen-conflicting-spacing
-  (gen/fmap (fn [[p v1 v2]] [(str p "-" v1) (str p "-" v2)])
-            (gen/tuple (gen/elements ["p" "m" "w" "h" "gap"])
-                       gen-spacing
-                       gen-spacing)))
-
 (def gen-non-conflicting
   (gen/one-of
    [(gen/fmap (fn [[v1 v2]] [(str "px-" v1) (str "py-" v2)])
-              (gen/tuple gen-spacing gen-spacing))
+              (gen/tuple g/gen-spacing-value g/gen-spacing-value))
     (gen/fmap (fn [[c s]] [(str "text-" c) (str "text-" s)])
-              (gen/tuple gen-color-value
+              (gen/tuple g/gen-color-value
                          (gen/elements ["xs" "sm" "base" "lg" "xl"])))
     (gen/tuple gen-display gen-position)]))
 
-;;; ----------------------------------------------------------------------------
-;;; Negative value generators
-
-(def gen-negative-spacing
-  (gen/fmap (fn [[p v]] (str "-" p "-" v))
-            (gen/tuple (gen/elements ["m" "mx" "my" "mt" "mr" "mb" "ml"
-                                      "top" "right" "bottom" "left"
-                                      "inset" "inset-x" "inset-y"])
-                       gen-spacing)))
-
-;;; ----------------------------------------------------------------------------
-;;; Stroke/outline color vs width (non-conflicting)
-
 (def gen-stroke-color-and-width
-  "Stroke color and width are different groups - should not conflict."
   (gen/fmap (fn [[color width]]
               [(str "stroke-" color) (str "stroke-" width)])
-            (gen/tuple gen-color-value gen-arbitrary-width)))
+            (gen/tuple g/gen-color-value g/gen-arbitrary-length)))
 
 (def gen-outline-color-and-width
-  "Outline color and width are different groups - should not conflict."
   (gen/fmap (fn [[color width]]
               [(str "outline-" color) (str "outline-" (inc width))])
-            (gen/tuple gen-color-value (gen/choose 1 8))))
-
-;;; ----------------------------------------------------------------------------
-;;; Important modifier generators
+            (gen/tuple g/gen-color-value (gen/choose 1 8))))
 
 (def gen-important-class
-  "Class with important modifier (either prefix or suffix)."
-  (gen/let [base gen-base-class
+  (gen/let [base g/gen-base-class
             style (gen/elements [:prefix :suffix])]
     (case style
       :prefix (str "!" base)
       :suffix (str base "!"))))
-
-;;; ----------------------------------------------------------------------------
-;;; Content utility generators
 
 (def gen-content-value
   (gen/elements ["['hello']" "[attr(data-content)]" "[var(--content)]" "['']"]))
@@ -182,17 +52,13 @@
   (gen/fmap (fn [[v1 v2]] [(str "content-" v1) (str "content-" v2)])
             (gen/tuple gen-content-value gen-content-value)))
 
-;;; ----------------------------------------------------------------------------
-;;; Cross-group conflict generators (hierarchical specificity)
-
 (def gen-inset-hierarchy
-  "Inset overrides inset-x/inset-y, which override individual sides."
-  (gen/let [v1 gen-spacing
-            v2 gen-spacing]
+  (gen/let [v1 g/gen-spacing-value
+            v2 g/gen-spacing-value]
     (gen/elements
-     [[(str "inset-x-" v1) (str "inset-" v2)]     ; inset wins
-      [(str "inset-y-" v1) (str "inset-" v2)]     ; inset wins
-      [(str "right-" v1) (str "inset-x-" v2)]     ; inset-x wins
+     [[(str "inset-x-" v1) (str "inset-" v2)]
+      [(str "inset-y-" v1) (str "inset-" v2)]
+      [(str "right-" v1) (str "inset-x-" v2)]
       [(str "left-" v1) (str "inset-x-" v2)]])))
 
 ;;; ----------------------------------------------------------------------------
@@ -212,23 +78,23 @@
 ;;; Properties
 
 (defspec idempotence num-tests
-  (for-all [classes gen-tailwind-class-list]
+  (for-all [classes g/gen-tailwind-class-list]
     (let [once  (resolve-classes classes)
           twice (resolve-classes [once])]
       (= once twice))))
 
 (defspec output-is-subset-of-input num-tests
-  (for-all [classes gen-tailwind-class-list]
+  (for-all [classes g/gen-tailwind-class-list]
     (let [result (result-classes (resolve-classes classes))
           input  (set classes)]
       (set/subset? result input))))
 
 (defspec last-color-wins num-tests
-  (for-all [[c1 c2] gen-conflicting-colors]
+  (for-all [[c1 c2] g/gen-conflicting-colors]
     (str/includes? (resolve-classes [c1 c2]) c2)))
 
 (defspec last-spacing-wins num-tests
-  (for-all [[c1 c2] gen-conflicting-spacing]
+  (for-all [[c1 c2] g/gen-conflicting-spacing]
     (str/includes? (resolve-classes [c1 c2]) c2)))
 
 (defspec last-display-wins num-tests
@@ -243,8 +109,8 @@
            (contains? result c2)))))
 
 (defspec modifiers-separate-groups num-tests
-  (for-all [base gen-base-class
-            mod  gen-modifier]
+  (for-all [base g/gen-base-class
+            mod  g/gen-modifier]
     (let [modified (str mod ":" base)
           result   (resolve-classes [base modified])]
       (and (str/includes? result base)
@@ -257,8 +123,8 @@
 (defspec negative-positive-conflict num-tests
   (for-all [prefix (gen/elements ["m" "mx" "my" "mt" "mr" "mb" "ml"
                                   "top" "right" "bottom" "left"])
-            v1     gen-spacing
-            v2     gen-spacing]
+            v1     g/gen-spacing-value
+            v2     g/gen-spacing-value]
     (let [neg (str "-" prefix "-" v1)
           pos (str prefix "-" v2)]
       (= pos (resolve-classes [neg pos])))))
